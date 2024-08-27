6e76232a878a5ebf624d296b64560a593c678d42
Nasih P S <nasihps@gmail.com>
2024-08-27 10:42:05 +0530

    added comment in controller

--- src/main/java/com/readyproject/samplegreet/Controller.java
+++ src/main/java/com/readyproject/samplegreet/Controller.java
@@ -4,7 +4,7 @@ package com.readyproject.samplegreet;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//comment
 @RestController
 @RequestMapping
 public class Controller {
